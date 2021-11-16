1f1c2021134fcfc77dc4e5dc18afa4eeab3a89c8
guillermoarre <guillermo.arredondo247@gmail.com>
2021-11-15 19:49:42 -0600

    what I made the day 1

---
+++ day1/e1.py
@@ -0,0 +1,25 @@
+#string
+string1 = "Hello world"
+#integer
+num1 = 3
+#float
+float1 = 3.1416
+#boolean
+flag1 = True
+flag2 = False
+
+"""""
+print(num1)
+print(f"Ineteger = {num1}")
+"""""
+#Pedir datos
+"""string2 = input("type a new string: ")
+print(string2)"""
+
+#Pedir datos diferentes de string
+a=int(input("Type a number: "))
+b=int(input("Type a number: "))
+
+c= a+b
+print(c)
+
---
+++ day1/e2.py
@@ -0,0 +1,7 @@
+#Band new generator
+print("Welcome to the band name generator")
+city = input("what's the name of teh city you grow up in?: ")
+pet = input("what's your pet name")
+
+band_name = city + "" + pet
+print(f"Your band could be {band_name}")
