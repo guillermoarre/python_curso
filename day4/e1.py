c094d57ddec73c82099e32c623d897644e1c5392
guillermoarre <guillermo.arredondo247@gmail.com>
2021-11-24 22:10:17 -0600

    What I made the day  4

---
+++ day4/e1.py
@@ -0,0 +1,22 @@
+#while loop 
+"""i=0
+while i<=10:
+    print(i)
+    i = i+1"""
+
+"""while input("Type a messagee: ").lower() != "stop":
+    print("hello")"""
+#list.reverse(list)
+"""list = [1,2,3,4,5,6]
+for position in list:
+    print(position)"""
+"""for number in range(0,10):
+    print(number)
+for char in "Hello world":
+    print(char)"""
+
+message1 = "hello"
+message2 = []
+for _ in message1:
+    message2.append(_)
+print(message2)
