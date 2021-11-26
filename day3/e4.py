6daf7a08269396124bb6227c27ced3f3dfa11d66
guillermoarre <guillermo.arredondo247@gmail.com>
2021-11-25 22:05:37 -0600

    What I made the day 3

--- day3/e4.py
+++ day3/e4.py
@@ -1,6 +1,48 @@
 #piedra, papel o tijera
 import random
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
 
-game = ['rock','scisors','sheet']
-jugado = random.choice(game)
-print(jugado)
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+game = [rock,paper,scissors]
+computer =random.randint(0,len(game)-1)
+choice = int(input("Make a choice 0-rock, 1-paper, 2-scissors: "))
+#print(game[choice])
+
+if choice == 0 and computer == 2:
+    print("User wins!!")
+elif choice == 2 and computer == 0:
+    print("Computer wins!!")
+elif choice == computer:
+    print("It is a tie!!")
+elif choice > computer:
+    print("User wins!!")
+elif choice < computer:
+    print("Computer wins!!")
+#Print
+print("User's choice: ")
+print(game[choice])
+print("Computer's choice: ")
+print(game[computer])
